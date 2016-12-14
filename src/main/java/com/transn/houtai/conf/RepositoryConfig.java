292e59f676a456598967230f057a0237613e7f2d
xuxiaohe <810579063@qq.com>
2016-12-14 13:55:44 +0800

    first

---
+++ src/main/java/com/transn/houtai/conf/RepositoryConfig.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright 2015 Brient Oh @ Pristine Core
+ * boh@pristinecore.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.transn.houtai.conf;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"com.transn.houtai.domain"})
+@EnableJpaRepositories(basePackages = {"com.transn.houtai.repository"})
+@EnableTransactionManagement
+public class RepositoryConfig {
+
+}
---
+++ src/main/java/com/transn/houtai/conf/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.transn.houtai.conf;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * ClassName: WebMvcConfiguration  
+ * (Springboot1.4.0有所变更)
+ * @author zhangtian  
+ * @version
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration extends WebMvcAutoConfiguration {
+
+}
